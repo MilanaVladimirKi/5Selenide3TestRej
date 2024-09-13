2b255a3838203255103897b3aa534874d0bac3fb
Milana Kichutkina <MilanaVladimir@mail.ru>
2024-09-13 09:17:30 +0500

    Tests 2

--- src/test/java/ru/topacademy/test/InetBankTest.java
+++ src/test/java/ru/topacademy/test/InetBankTest.java
@@ -31,4 +31,24 @@ public class InetBankTest {
         $("h2").shouldHave(exactText("Личный кабинет"));
     }
 
+    @Test
+    void shouldErrorIfNotRegisteredUser() {
+        var notRegisteredUser = getUser("active");
+        $("[data-test-id=login] input").setValue(notRegisteredUser.getLogin());
+        $("[data-test-id=password] input").setValue(notRegisteredUser.getPassword());
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification] .notification__title").shouldHave(exactText("Ошибка"), Duration.ofSeconds(10));
+        $("[data-test-id=error-notification] .notification__content").shouldHave(exactText("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(10));
+    }
+
+    @Test
+    void shouldErrorIfBlockedUser() {
+        var blockedUser = getUser("blocked");
+        $("[data-test-id=login] input").setValue(blockedUser.getLogin());
+        $("[data-test-id=password] input").setValue(blockedUser.getPassword());
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification] .notification__title").shouldHave(exactText("Ошибка"), Duration.ofSeconds(10));
+        $("[data-test-id=error-notification] .notification__content").shouldHave(exactText("Ошибка! Неверно указан логин или пароль"), Duration.ofSeconds(10));
+    }
+
 }
